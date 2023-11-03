7a9a1b1d46fa293e3f779754d19ba60f9ca2e314
LROsipov <osipov.l.r@yandex.ru>
2023-11-03 17:25:56 +0300

    fix

--- src/test/java/site/stellaburgers/test/OrdersTest.java
+++ src/test/java/site/stellaburgers/test/OrdersTest.java
@@ -28,7 +28,7 @@ public class OrdersTest extends BaseTest {
     @DisplayName("Получение заказов без авторизации")
     public void getOrdersNotAuthorisedTest() {
         userResponse = orderApiSteps.sendGetOrder("");
-        step("Проверить, что  статус код 400", () -> {
+        step("Проверить, что  статус код 401", () -> {
             userResponse.then().statusCode(401);
         });
         step("Проверить, что  в теле ответа [message] -" + AUTHORISED_FAIL.getMessage(), () -> {
@@ -40,7 +40,7 @@ public class OrdersTest extends BaseTest {
     @DisplayName("Получение заказов авторизированного пользователя")
     public void getOrdersAuthorisedTest() {
         userResponse = orderApiSteps.sendGetOrder(pair.getLeft());
-        step("Проверить, что  статус код 400", () -> {
+        step("Проверить, что  статус код 200", () -> {
             userResponse.then().statusCode(200);
         });
         step("Проверить, что тело ответа  содержит [success] со значением true", () -> {
